1be19b71083837f4e69d08318b82385027b134c2
RobertCox972 <107111535+RobertCox972@users.noreply.github.com>
2025-09-07 22:50:58 +0100

    Add error handling for non-POST requests in quote.php

--- quote.php
+++ quote.php
@@ -1,4 +1,8 @@
 <?php
-if
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+    exit;
+}
 echo"$_POST[\"email\"]";
 ?>
