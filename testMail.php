c35595c645b06ba5d03866b999286729c547b1e4
RobertCox972 <107111535+RobertCox972@users.noreply.github.com>
2025-11-24 03:05:57 +0000

    email errors

--- testMail.php
+++ testMail.php
@@ -1,31 +1,33 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// If using Composer
 require __DIR__ . '/vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
-
 $mail = new PHPMailer(true);
+
 try {
     $mail->isSMTP();
     $mail->Host       = 'mail.bikesinavan.co.uk';
     $mail->SMTPAuth   = true;
     $mail->Username   = 'info@bikesinavan.co.uk';
     $mail->Password   = 'emailPassword';
-    $mail->SMTPSecure = 'tls';
-    $mail->Port       = 465;
+    $mail->SMTPSecure = 'tls';  // use 'ssl' if port 465
+    $mail->Port       = 587;    // use 465 if SSL
 
-    $mail->setFrom('you@bikesinavan.co.uk', 'BikesInAVan');
+    $mail->setFrom('info@bikesinavan.co.uk', 'BikesInAVan');
     $mail->addAddress('youremail@example.com');
 
     $mail->Subject = 'Test email';
     $mail->Body    = 'This is a test email from BikesInAVan quote page.';
     $mail->send();
+
     echo "Email sent successfully!";
 } catch (Exception $e) {
     echo "Mailer Error: " . $mail->ErrorInfo;
 }
-
