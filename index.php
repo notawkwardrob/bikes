51a150a8f1966a6c4bc6a3b58442ca5c046b8e4d
RobertCox972 <107111535+RobertCox972@users.noreply.github.com>
2025-11-25 22:48:29 +0000

    Update index.php

--- index.php
+++ index.php
@@ -111,7 +111,7 @@ header { display:flex; flex-wrap:wrap; justify-content:space-between; align-item
 
 <header>
     <div class="logo">
-        <img src="newLogo.png" alt="BikesInAVan logo"/>
+        <img src="/images/newLogo.png" alt="BikesInAVan logo"/>
         <div class="brand">
             <h1>bikesinavan.co.uk</h1>
             <p>Secure motorcycle transport.</p>
